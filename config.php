ff0d0f7fa918031fc87920c8833ad33eeba59156
hmonaghan <me@hez.ninja>
2022-07-07 10:47:41 +1000

    added my prismic

--- config.php
+++ config.php
@@ -9,7 +9,7 @@
 /*
  * Change this for the URL of your repository.
  */
-define('PRISMIC_URL', 'https://your-repo-name.prismic.io/api/v2');
+define('PRISMIC_URL', 'https://thisisperiscope-test.prismic.io/api/v2');
 define('PRISMIC_TOKEN', null);
 
 /*
